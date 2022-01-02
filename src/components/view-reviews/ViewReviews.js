9a44d2fbc119e9af415d9ab929af29f8df4fca86
khaismah khan <khaismahilyaskhan00@gmail.com>
2022-01-03 02:47:40 +0500

    view reviews screen

--- src/components/view-reviews/ViewReviews.js
+++ src/components/view-reviews/ViewReviews.js
@@ -7,6 +7,9 @@ import {
     ScrollView,
 } from "react-native";
 import Header from "../../components/header";
+import { CommonButton } from "../buttons";
+import { PostsReviews } from "./components/postsReviews";
+import { ViewReviewsHeader } from "./components/ViewReviewsHeader";
 
 
 
@@ -16,7 +19,14 @@ const ViewReviews = ({ navigation }) => {
         <View style={{ flex: 1, backgroundColor: "white" }}>
             <Header headingText="Product's Review" />
             <ScrollView>
-                <Text>View Reviews</Text>
+
+                <View style={{ alignItems: 'center' }}>
+                    
+                   
+                    <View style={{ width: "90%" }}>
+                        <CommonButton text="Load More" />
+                    </View>
+                </View>
 
             </ScrollView>
 
