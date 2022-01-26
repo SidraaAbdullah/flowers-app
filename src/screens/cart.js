eb91f4b0c09306a3cb5dcdf2542b11f538142695
SidraAbdullah <sidraabdullah56@gmail.com>
2022-01-26 23:24:30 +0500

    Cart screen updates

--- src/screens/cart.js
+++ src/screens/cart.js
@@ -6,8 +6,14 @@ import { useSelector } from "react-redux";
 const CartScreen = ({ navigation }) => {
   const cartItems = useSelector((state) => state.cart.addToCart);
 
-  return cartItems.length ? (
-    <Cart navigation={navigation} cartItems={cartItems} />
+  //REMOVE DUPLICATE OBJECT FROM CART ITEMS
+  const values = cartItems
+    .reduce((map, obj) => map.set(obj._id, obj), new Map())
+    .values();
+  const uniqueCartItems = [...values];
+
+  return uniqueCartItems.length ? (
+    <Cart navigation={navigation} cartItems={uniqueCartItems} />
   ) : (
     <EmptyCart navigation={navigation} />
   );
