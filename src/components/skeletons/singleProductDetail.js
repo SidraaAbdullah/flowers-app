b5a1097d6996aa03231899e08cb1824bed65bb3e
khaismah khan <khaismahilyaskhan00@gmail.com>
2022-02-06 04:14:07 +0500

    single product skeleton

---
+++ src/components/skeletons/singleProductDetail.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { View, StyleSheet, Dimensions, Text, Image } from 'react-native';
+const SingleProductDetail = () => {
+    return (
+        <View style={styles.border}>
+            <View style={{ alignItems: "center" }}>
+                <Image
+                    style={styles.bgColorImage}
+
+                />
+            </View>
+
+            <View style={{ paddingVertical: 6, paddingHorizontal: 2 }}>
+                <Text
+                    style={styles.name}
+
+                >
+
+                </Text>
+                <Text
+                    style={styles.price}
+                >
+
+                </Text>
+                <View style={{display:'flex', flexDirection:'row'}}>
+                <Text
+                    style={styles.star}
+                >
+
+                </Text>
+                <Text
+                    style={styles.add}
+                >
+
+                </Text>
+                </View>
+            </View>
+        </View>
+    );
+};
+export { SingleProductDetail };
+
+const styles = StyleSheet.create({
+    border: {
+        borderWidth: 1,
+        justifyContent: "center",
+        borderStyle: "dashed",
+        borderRadius: 8,
+        borderColor: "gray",
+        paddingTop: 10,
+        paddingBottom: 6,
+        paddingHorizontal: 8,
+        backgroundColor: "white",
+    },
+    bgColorImage: {
+        backgroundColor: 'lightgray',
+        width: 100, height: 100,
+        borderRadius:5
+    },
+    name: {
+        backgroundColor: 'lightgray',
+        margin: 1,
+        width:60
+    },
+    price:{
+        backgroundColor: 'lightgray',
+        width:45,
+        margin:1
+    },
+    bgColor: {
+        backgroundColor: 'lightgray',
+        margin: 1,
+    },
+    star:{
+        backgroundColor: 'lightgray',
+        width:105,
+        marginRight:15,
+        marginTop:5,
+        height:20
+    },
+    add:{
+        width: 30, height: 30,
+        backgroundColor: 'lightgray',
+        borderRadius:50
+    }
+});
