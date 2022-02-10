d70d33c4f334ddbfa758fcb1436dbd19998ff247
Muhammad Salman Siddiqui <43972004+MuhammadSalmanSiddiqui@users.noreply.github.com>
2022-02-10 08:47:29 +0500

    order cancel

--- src/components/order-history/components/OrderList.js
+++ src/components/order-history/components/OrderList.js
@@ -8,99 +8,118 @@ const OrderList = ({ status, navigation, item }) => {
   useEffect(() => {
     getOrderStatus(item, setResponse);
   }, []);
+  console.log(item);
 
   return (
-    <TouchableOpacity
-      onPress={() => navigation.navigate("singleOrderHistory", { item: item })}
-    >
-      <View style={styles.border}>
-        <View
-          style={{
-            padding: 10,
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <View style={{ width: "30%" }}>
-            <Image
-              style={{ width: 80, height: 80 }}
-              source={{
-                uri: "https://bakeryonline.pk/wp-content/uploads/2020/08/bouquet-of-1-dozen-roses.jpg",
-              }}
-            />
-          </View>
-          <View style={{ width: "70%" }}>
-            <View style={[styles.container, { marginBottom: 3 }]}>
-              <Text style={styles.font}>Rose (2 items)</Text>
-              <Text style={styles.font}>$40.00</Text>
+    <>
+      {item?.status == "DELIVERED" && (
+        <TouchableOpacity onPress={() => navigation.navigate("reviewScreen")}>
+          <Text
+            style={{
+              color: "red",
+              fontFamily: "ProximaNovaSemiBold",
+              textAlign: "right",
+              marginVertical: 6,
+            }}
+          >
+            Review
+          </Text>
+        </TouchableOpacity>
+      )}
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("singleOrderHistory", { item: item })
+        }
+      >
+        <View style={styles.border}>
+          <View
+            style={{
+              padding: 10,
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <View style={{ width: "30%" }}>
+              <Image
+                style={{ width: 80, height: 80 }}
+                source={{
+                  uri: "https://bakeryonline.pk/wp-content/uploads/2020/08/bouquet-of-1-dozen-roses.jpg",
+                }}
+              />
             </View>
-            <Text
-              numberOfLines={1}
-              style={{
-                fontFamily: "ProximaNova",
-                fontSize: 14,
-                color: "gray",
-              }}
-            >
-              Loreum ipsum dolor sit amet, consectetur non adipiscing elit.
-            </Text>
-            <View
-              style={{
-                alignItems: "center",
-                flexDirection: "row",
-                marginVertical: 3,
-                marginBottom: 8,
-              }}
-            >
+            <View style={{ width: "70%" }}>
+              <View style={[styles.container, { marginBottom: 3 }]}>
+                <Text style={styles.font}>Rose (2 items)</Text>
+                <Text style={styles.font}>$40.00</Text>
+              </View>
               <Text
+                numberOfLines={1}
                 style={{
                   fontFamily: "ProximaNova",
+                  fontSize: 14,
                   color: "gray",
-                  marginRight: 3,
                 }}
               >
-                Invoice No:
+                Loreum ipsum dolor sit amet, consectetur non adipiscing elit.
               </Text>
-              <Text style={{ fontFamily: "ProximaNova", color: "red" }}>
-                39303030
-              </Text>
-            </View>
-            <View style={styles.container}>
               <View
                 style={{
-                  justifyContent: "center",
                   alignItems: "center",
                   flexDirection: "row",
+                  marginVertical: 3,
+                  marginBottom: 8,
                 }}
               >
-                <Icon name="calendar-outline" size={18} type="ionicon" />
                 <Text
                   style={{
                     fontFamily: "ProximaNova",
-                    marginLeft: 5,
-                    marginTop: 2,
+                    color: "gray",
+                    marginRight: 3,
                   }}
                 >
-                  27 May
+                  Invoice No:
+                </Text>
+                <Text style={{ fontFamily: "ProximaNova", color: "red" }}>
+                  39303030
                 </Text>
               </View>
+              <View style={styles.container}>
+                <View
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flexDirection: "row",
+                  }}
+                >
+                  <Icon name="calendar-outline" size={18} type="ionicon" />
+                  <Text
+                    style={{
+                      fontFamily: "ProximaNova",
+                      marginLeft: 5,
+                      marginTop: 2,
+                    }}
+                  >
+                    27 May
+                  </Text>
+                </View>
 
-              <Text
-                style={{
-                  fontFamily: "ProximaNovaBold",
-                  marginTop: 2,
-                  color: "green",
-                  fontSize: 16,
-                }}
-              >
-                {response}
-              </Text>
+                <Text
+                  style={{
+                    fontFamily: "ProximaNovaBold",
+                    marginTop: 2,
+                    color: "green",
+                    fontSize: 16,
+                  }}
+                >
+                  {response}
+                </Text>
+              </View>
             </View>
           </View>
         </View>
-      </View>
-    </TouchableOpacity>
+      </TouchableOpacity>
+    </>
   );
 };
 export { OrderList };
